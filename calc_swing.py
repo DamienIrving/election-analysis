fa9c166416bf6d6474060e94e1527781665d3ead
Damien Irving <irving.damien@gmail.com>
2021-03-22 07:41:41 +1100

    Swing votes for State18

--- calc_swing.py
+++ calc_swing.py
@@ -22,9 +22,23 @@ def main(args):
                         on=['PollingPlaceNm'],
                         suffixes=('_' + args.recent_year, '_' + args.old_year),)
                         
-    swing_df['Swing'] = swing_df['GreensPercentage_' + args.recent_year] - \
-                        swing_df['GreensPercentage_' + args.old_year]
+    swing_df['SwingPercentage'] = swing_df['GreensPercentage_' + args.recent_year] - \
+                                  swing_df['GreensPercentage_' + args.old_year]
     
+    if not args.verbose:
+        swing_df = swing_df[['PollingPlaceNm',
+                             'DivisionNm',
+                             'GreensVotes_' + args.recent_year,
+                             'TotalVotes_' + args.recent_year,
+                             'GreensPercentage_' + args.recent_year,
+                             'SwingPercentage',
+                             'PremisesNm_' + args.recent_year,
+                             'PremisesAddress_' + args.recent_year,
+                             'Latitude',
+                             'Longitude',
+                             'LegCo',
+                             'LocalCouncil']]
+                             
     swing_df.to_csv(args.outfile, index=False)
     
     
@@ -36,7 +50,8 @@ if __name__ == '__main__':
     parser.add_argument("recent_year", type=str, help="year of the recent election")
     parser.add_argument("old_election_csv", type=str, help="vote file for recent election")
     parser.add_argument("old_year", type=str, help="year of the old election")
-    
-    parser.add_argument("outfile", type=str, help="Output file")    
+    parser.add_argument("outfile", type=str, help="Output file")
+    parser.add_argument("--verbose", action="store_true", default=False,
+                        help="Include more old election information in output")    
     args = parser.parse_args()
     main(args)
